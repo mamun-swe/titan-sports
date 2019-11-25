3fe4f46931b4a6ddcbede7e9b37ed4354afa58a0
Mamun-swe <mhmamun166009@gmail.com>
2019-11-26 02:35:45 +0600

    make it better

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.get(/.*/, function (req, res) {
+    res.sendFile(__dirname + "/dist/index.html");
+})
+
+const port = process.env.PORT || 8080
+app.listen(port)
