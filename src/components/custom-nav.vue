525ceab27eb77ef2cef9290d57af461aecbe2c45
Mamun-swe <mhmamun166009@gmail.com>
2019-11-26 03:03:17 +0600

    first push

---
+++ src/components/custom-nav.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="custom-nav">
+        
+    </div>
+</template>
+<script>
+export default {
+    name: 'custom-nav'
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
